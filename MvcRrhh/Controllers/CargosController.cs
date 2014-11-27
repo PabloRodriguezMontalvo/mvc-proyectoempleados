23c921c155b1fcebd23d026cd369631e41b2c9e4
luisgiltajamar <luis.gil@tajamar365.com>
2014-11-27 09:30:24 +0100

    a

---
+++ MvcRrhh/Controllers/CargosController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRrhh.Controllers
+{
+    public class CargosController : BaseController
+    {
+        // GET: Cargos
+        public ActionResult Index()
+        {
+
+            return View(RepositorioCargo.Get());
+        }
+
+        public ActionResult BuscarCargo(String nombre)
+        {
+            var data = RepositorioCargo.GetByNombre(nombre);
+            return PartialView("_cargos",data);
+        }
+
+          
+    }
+}
